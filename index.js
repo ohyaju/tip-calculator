c96e6690bfae1fcc0ce7da970d9ee8e2d7d3c00e
ohyaju <bnomin257@gmail.com>
2025-01-22 14:30:00 +0800

    sainu

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
-const tipsRef = document.getElementsByClassName('tips'); // []
+const tipsRef = document.getElementsByClassName('tips'); 
 const inputRef = document.getElementById('input');
-let tip = ''
-for (let i = 0; i < tipsRef.length; i++) {
-    tipsRef[i].addEventListener('click', (event) => {
-        console.log() // 10%
+const total = document.getElementById('total')
 
-        tip = inputRef.value * event.target.textContent.slice(0, 2) / 100
-        const total = Number(tip) + Number(inputRef.value)
-        document.getElementById("total").innerHTML = total
+inputRef.addEventListener('input', function(){
+    console.log(typeof inputRef.value,  tipsRef[0].textContent)
+})
 
-    });
-    // document.getElementById("reset").onclick = function () {
-    //     inputRef.value = ""};
-} // total
 
-document.getElementById("reset").onclick = function () {
-    inputRef.value = ""};
- // reset button 
+
+ 
+tipsRef[0].addEventListener('click', function(){
+  
+   total.innerHTML  = inputRef.value * tipsRef[0].textContent
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for (let i = 0; i < tipsRef.length; i++) {
+//    tipsRef[i].addEventListener('click', (event) => {
+//        console.log('hello') // 10%
+//
+//        tip = inputRef.value * event.target.textContent.slice(0, 2) / 100
+//        const total = Number(tip) + Number(inputRef.value)
+//        document.getElementById("total").innerHTML = total
+//
+//    });
+//    // document.getElementById("reset").onclick = function () {
+//    //     inputRef.value = ""};
+//} // total
+//
+//document.getElementById("reset").onclick = function () {
+//    inputRef.value = ""};
+// // reset button 
 
 
 

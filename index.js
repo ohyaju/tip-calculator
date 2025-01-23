6773db10ac1f7476143272bbaa179bea83213c5b
ohyaju <bnomin257@gmail.com>
2025-01-23 11:11:43 +0800

    calculator gadna zasvar

--- index.js
+++ index.js
@@ -1,58 +1,38 @@
-const tipsRef = document.getElementsByClassName('tips'); 
+const tipsRef = document.getElementsByClassName('tips');
 const inputRef = document.getElementById('input');
 const total = document.getElementById('total')
 
-inputRef.addEventListener('input', function(){
-    console.log(typeof inputRef.value,  tipsRef[0].textContent)
-})
+// inputRef.addEventListener('input', function(){
+//     console.log(typeof inputRef.value,  tipsRef[0].textContent)
+// })
+
+// tipsRef[0].addEventListener('click', function(){
+
+//    total.innerHTML  = inputRef.value * tipsRef[0].textContent
+
+// })
+
+for (let i = 0; i < tipsRef.length; i++) {
+    tipsRef[i].addEventListener('click', (event) => {
+        console.log('hello') // 10%
+
+        tip = inputRef.value * event.target.textContent.replace('%', '') / 100
+        console.log(tip, inputRef.value);
+
+        total.innerHTML = Number(tip) + Number(inputRef.value)
 
 
+    });
+    // document.getElementById("reset").onclick = function () {
+    //     inputRef.value = ""};
+} // total
 
- 
-tipsRef[0].addEventListener('click', function(){
-  
-   total.innerHTML  = inputRef.value * tipsRef[0].textContent
-
-})
-
-
-
-
+document.getElementById("reset").onclick = function () {
+    inputRef.value = ""
+    total.innerText = '0'
+}; // reset button 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//for (let i = 0; i < tipsRef.length; i++) {
-//    tipsRef[i].addEventListener('click', (event) => {
-//        console.log('hello') // 10%
-//
-//        tip = inputRef.value * event.target.textContent.slice(0, 2) / 100
-//        const total = Number(tip) + Number(inputRef.value)
-//        document.getElementById("total").innerHTML = total
-//
-//    });
-//    // document.getElementById("reset").onclick = function () {
-//    //     inputRef.value = ""};
-//} // total
-//
-//document.getElementById("reset").onclick = function () {
-//    inputRef.value = ""};
-// // reset button 
 
 
 
